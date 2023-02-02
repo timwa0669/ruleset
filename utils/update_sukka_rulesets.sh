3fb20fe05a2f6ce6f2172903c7dcf80418ccb9ac
timwa0669 <timwa0669@hotmail.com>
2023-02-02 21:05:14 +0800

    Update ruleset provider Switch reject source from Sukka to acl4ssr to lower misrejection.

--- utils/update_sukka_rulesets.sh
+++ utils/update_sukka_rulesets.sh
@@ -6,17 +6,15 @@ mkdir -p $WD
 cd $WD
 git clone ssh://git@github.com/timwa0669/ruleset.git
 cd ruleset/ruleset
-echo "# Patch: Improved compatibility with subconverter" > SukkaRejectBase.list
 echo "# Patch: Improved compatibility with subconverter" > SukkaRejectCustom.list
-curl -fsL https://ruleset.skk.moe/List/domainset/reject.conf >> SukkaRejectBase.list
 curl -fsL https://ruleset.skk.moe/List/domainset/reject_sukka.conf >> SukkaRejectCustom.list
 curl -fsL https://ruleset.skk.moe/List/ip/reject.conf > SukkaRejectIP.list
-sed -i 's/^/DOMAIN,/g' SukkaRejectBase.list SukkaRejectCustom.list
-sed -i 's/^DOMAIN,\./DOMAIN-SUFFIX,/g' SukkaRejectBase.list SukkaRejectCustom.list
-sed -i 's/^DOMAIN,#/#/g' SukkaRejectBase.list SukkaRejectCustom.list
-sed -i 's/^DOMAIN,$//g' SukkaRejectBase.list SukkaRejectCustom.list
-git add SukkaRejectBase.list SukkaRejectCustom.list
-git commit -m 'Bot: Update Sukka rulesets'
+sed -i 's/^/DOMAIN,/g' SukkaRejectCustom.list
+sed -i 's/^DOMAIN,\./DOMAIN-SUFFIX,/g' SukkaRejectCustom.list
+sed -i 's/^DOMAIN,#/#/g' SukkaRejectCustom.list
+sed -i 's/^DOMAIN,$//g' SukkaRejectCustom.list
+git add SukkaRejectCustom.list SukkaRejectIP.list
+git commit -m 'Bot: Update Sukka ruleset'
 git push
 cd ../../../
 rm -rf $WD
