fee2f3ac1c3b46fe7eccb30a7b015b0720a126a5
timwa0669 <timwa0669@hotmail.com>
2023-06-19 07:58:14 +0800

    Update linovelib rewrites

--- scripts/linovelib.js
+++ scripts/linovelib.js
@@ -1,2 +1,2 @@
-let body = $response.body.replace(/<script((?!<\/script>).)*(window\.adblock=\!0|adsbygoogle|googlesyndication|googletagmanager)((?!<\/script>).)*<\/script>/gs, '');
+let body = $response.body.replace(/<script((?!<\/script>).)*(window\.adblock=\!0|adsbygoogle|googlesyndication|googletagmanager|sojson)((?!<\/script>).)*<\/script>/gs, '');
 $done({ body });
