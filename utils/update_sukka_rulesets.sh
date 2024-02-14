d77f202e0565bb277107a4f776da65e41b89258d
timwa0669 <timwa0669@hotmail.com>
2024-02-14 13:24:53 +0800

    remove deprecated SukkaRejectCustom.list

--- utils/update_sukka_rulesets.sh
+++ utils/update_sukka_rulesets.sh
@@ -6,14 +6,8 @@ mkdir -p $WD
 cd $WD
 git clone ssh://git@github.com/timwa0669/ruleset.git
 cd ruleset/ruleset
-echo "# Patch: Improved compatibility with subconverter" > SukkaRejectCustom.list
-curl -fsL https://ruleset.skk.moe/List/domainset/reject_sukka.conf >> SukkaRejectCustom.list
 curl -fsL https://ruleset.skk.moe/List/ip/reject.conf > SukkaRejectIP.list
-sed -i 's/^/DOMAIN,/g' SukkaRejectCustom.list
-sed -i 's/^DOMAIN,\./DOMAIN-SUFFIX,/g' SukkaRejectCustom.list
-sed -i 's/^DOMAIN,#/#/g' SukkaRejectCustom.list
-sed -i 's/^DOMAIN,$//g' SukkaRejectCustom.list
-git add SukkaRejectCustom.list SukkaRejectIP.list
+git add SukkaRejectIP.list
 git commit -m 'Bot: Update Sukka ruleset'
 git push
 cd ../../../
