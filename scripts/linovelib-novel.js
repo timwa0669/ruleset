d908160d5e6e55142dc43482d0826a73419b8251
timwa0669 <timwa0669@hotmail.com>
2025-03-22 19:49:19 +0800

    Update linovelib rewrite

--- scripts/linovelib-novel.js
+++ scripts/linovelib-novel.js
@@ -1,2 +1,2 @@
-let body = $response.body.replace(/<script((?!(<\/script>)|(prevpage)|(nextpage)|(bookpage)).)*<\/script>/gs, '');
+let body = $response.body.replace(/<script((?!(<\/script>)|(prevpage)|(nextpage)|(bookpage)|(ReadParams)).)*<\/script>/gs, '');
 $done({ body });
