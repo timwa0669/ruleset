d8f7909a49e9f56c6d119d7eec92e2cbc7296bc9
timwa0669 <timwa0669@hotmail.com>
2025-06-05 20:47:49 +0800

    Update

--- scripts/linovelib-novel.js
+++ scripts/linovelib-novel.js
@@ -1,2 +1,3 @@
-let body = $response.body.replace(/<script((?!(<\/script>)|(prevpage)|(nextpage)|(bookpage)|(ReadParams)).)*<\/script>/gs, '');
+//let body = $response.body.replace(/<script((?!(<\/script>)|(prevpage)|(nextpage)|(bookpage)|(ReadParams)).)*<\/script>/gs, '');
+let body = $response.body.replace(/<script((?!<\/script>).)*(window\.adblock=\!0|adsbygoogle|googlesyndication|googletagmanager|sojson|yuedu|tj|show-more-images|serviceWorker|tip_chapter|readtools|zation|baidu)((?!<\/script>).)*<\/script>/gs, '');
 $done({ body });
