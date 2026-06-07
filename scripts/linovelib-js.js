495d6a9e7829408b381b891a9e5ed3b767eb6388
timwa0669 <timwa0669@hotmail.com>
2026-06-07 11:15:54 +0800

    Update linovelib

--- scripts/linovelib-js.js
+++ scripts/linovelib-js.js
@@ -1,3 +1,3 @@
 //let body = $response.body.replace(/<script((?!<\/script>).)*(window\.adblock=\!0|adsbygoogle|googlesyndication|googletagmanager|sojson)((?!<\/script>).)*<\/script>/gs, '');
-let body = $response.body.replace(/.*(\(function\(_0x|eval\(function\(p,a,c,k,e,d\)|AdBlock).*/g, '');
+let body = $response.body.replace(/.*(\(function\(_0x|eval\(function\(p,a,c,k,e,d\)).*/g, '');
 $done({ body });
